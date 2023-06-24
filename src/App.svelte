723c5225e642ada0759c4719f87d95c3c1d696f0
TimothySamson <timothy.gab.samson@gmail.com>
2023-06-23 21:49:12 -0700

    welcome onboard

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 
-  console.log("yo baby wassuppp");
 </script>
 
 <main>
@@ -14,7 +13,7 @@
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
-  </div>
+  </div >
   <h1>Vite + Svelte</h1>
 
   <div class="card">
@@ -23,11 +22,11 @@
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, 
-    the official Svelte app framework powered by Vite!
+    the official Svelte app framework powered by Vite and Lexington!
   </p>
 
   <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+    Click on the Vite and Svelte logos to learn more, you bloody mongrel
   </p>
 </main>
 
